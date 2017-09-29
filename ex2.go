b3cea025c94b344c66f729b1e84208ba85c6de90
kimburke55 <kimburke55@gmail.com>
2017-09-29 15:27:31 +0100

    Current Date and Time

---
+++ ex2.go
@@ -0,0 +1,15 @@
+// Kimberly Burke
+// G00269948
+//https://tour.golang.org/welcome/4
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//print current time and date
+	fmt.Println("The date and time is:", time.Now())
+	
+}
